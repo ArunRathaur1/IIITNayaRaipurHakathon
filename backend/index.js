d8be0b7d5aa4a4ca2b68756e674f63ec4ebc9a82
ArunRathaur1 <arunrathaur92.6@gmail.com>
2025-03-28 23:02:40 +0530

    updated the port value

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/ai", airoute);
 app.use('/api/payment', paymentRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
